eec64ab26948aac469607930f2bf6cf54ba3d61e
unknown <heji30590@gmail.com>
2026-01-12 23:38:25 +0900

    App.jsxを作成した

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import CompanyList from './CompanyList';
+
+function App() {
+  return (
+    <div>
+      <h1>AŠˆŠÇ—ƒAƒvƒŠ</h1>
+      <CompanyList />
+    </div>
+  );
+}
+
+export default App;
